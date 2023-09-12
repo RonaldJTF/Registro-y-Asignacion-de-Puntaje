a460e7c1511ae54e90a0bbfe12e84e54a97c0efc
Ronald <torresfloronald@gmail.com>
2023-09-12 14:29:01 -0500

    Estructura Inicial

---
+++ ciadti-especifico-rap-services/src/main/java/co/edu/unipamplona/ciadti/rap/services/CiadtiEspecificoRapServicesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unipamplona.ciadti.rap.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiadtiEspecificoRapServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiadtiEspecificoRapServicesApplication.class, args);
+	}
+
+}
---
+++ ciadti-especifico-rap-services/src/test/java/co/edu/unipamplona/ciadti/rap/services/CiadtiEspecificoRapServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unipamplona.ciadti.rap.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiadtiEspecificoRapServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
